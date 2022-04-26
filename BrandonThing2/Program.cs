a38e607229b6591e6d7b0ea2e265db20c19321dd
kent10000 <aaronrichards4321@gmail.com>
2022-04-25 23:19:20 -0400

    meese

--- BrandonThing2/Program.cs
+++ BrandonThing2/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int pzombibites;
+Console.WriteLine();
+Console.WriteLine("How many zombies are there on day 1");
+var day1zombies = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("How many people can they bite each day");
+var zombiebites = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+while (int.TryParse(zombiebites, out pzombibites) == false)
+{
+    Console.WriteLine("Try Again");
+    zombiebites = Console.ReadLine();
+}
+
+;
+
+Console.WriteLine();
+Console.WriteLine("How many days will the invasion last");
+var invasionduration = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+
+/*for (var days = 1; days <= invasionduration && day1zombies * int.Parse(zombiebites) <= 1000000; days++)
+{
+    day1zombies = day1zombies * zombiebitesp + day1zombies;
+    Console.WriteLine("There are " + day1zombies + " zombies at the end of day " + days);
+}*/
+
+
+
